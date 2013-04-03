b4867af318cf832c5cf3f029caba89b2ec8856eb
James Harton <jamesotron@gmail.com>
2013-04-04 12:39:14 +1300

    Get rid of abstract Runner class, YAGNI.

--- lib/rubby/transpiler.rb
+++ lib/rubby/transpiler.rb
@@ -1,9 +1,41 @@
 module Rubby
-  class Transpiler < Runner
+  class Transpiler
     require 'rubby/transpiler/ruby_formatter'
+    attr_accessor :source
+    attr_accessor :filename
+
+    def initialize(source, filename='STDIN', target_version=nil)
+      self.source = source
+      self.filename = filename
+      self.target = target_version
+    end
+
+    def tokens
+      @tokens ||= Rubby::Lexer.lex(source, filename)
+    end
+
+    def tree
+      @tree ||= tree_constructor
+    end
 
     def process
       RubyFormatter.process(tree.map { |n| n.to_ruby(self) })
+    end
+
+    def target=(x)
+      @target = TargetVersion.new(x.to_s)
+    end
+
+    def target
+      @target ||= TargetVersion.new
+    end
+
+    private
+
+    def tree_constructor
+      Rubby::Parser.parse(tokens).each do |node|
+        node.walk(node.children, self)
+      end
     end
   end
 end
--- spec/lib/rubby/transpiler_spec.rb
+++ spec/lib/rubby/transpiler_spec.rb
@@ -1,14 +1,45 @@
 require 'spec_helper'
 
 describe Rubby::Transpiler do
-  let(:tree) { stub(:tree) }
   let(:source) { "1" }
   let(:runner) { Rubby::Transpiler.new(source) }
-  before { runner.stub(:tree => tree) }
   subject { runner }
 
+  it { should respond_to(:source) }
+  it { should respond_to(:process) }
+  it { should respond_to(:tokens) }
+  it { should respond_to(:tree) }
   it { should respond_to(:target=) }
   it { should respond_to(:target) }
   it { should respond_to(:process) }
 
+  describe '#tokens' do
+    subject { runner.tokens }
+    it 'lexes the source' do
+      Rubby::Lexer.should_receive(:lex).with(source, 'STDIN')
+      subject
+    end
+  end
+
+  describe '#tree' do
+    subject { runner.tree }
+    it 'parses the tokens' do
+      tokens = Rubby::Lexer.lex(source)
+      runner.stub(:tokens => tokens)
+      Rubby::Parser.should_receive(:parse).with(tokens).and_return([])
+      subject
+    end
+  end
+
+  describe '#target' do
+    subject { runner.target }
+    it { should be_a(Rubby::TargetVersion) }
+  end
+
+  describe '#target=' do
+    it 'assigns the specified target version' do
+      subject.target = '1.2.3p1'
+      subject.target.to_s.should eq('1.2.3p1')
+    end
+  end
 end
